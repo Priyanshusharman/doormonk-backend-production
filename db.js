ab819a5f52ff465c94b0292e9009250d7be72d1d
priyanshusharman <priyanshuk1900@gmaul.com>
2023-11-28 23:14:45 +0530

    final change

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://0.0.0.0:27017/doormonk";
+const mongoURI = "mongodb+srv://Doormonk:Tleklu0wPvqOjuSG@doormonk.ajlmam6.mongodb.net/?retryWrites=true&w=majority";
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI)
-        .then(console.log("Connected To Mongo Successfully"));
+    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 10000 })
+    .then(console.log("Connected To Mongo Successfully"));
 }
+
+
+
 
 module.exports = connectToMongo;
